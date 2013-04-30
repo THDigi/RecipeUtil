32ecfb210b09dbb5b638c2f6b2671e77beb3247c
Digi <hunter_digital_sl@yahoo.com>
2013-04-30 14:05:55 +0300

    Fixed furnace ingredient checking.
    Minecraft only stores ingredient type for furnace recipes, data is ignored.

--- ro/thehunters/digi/recipeUtil/RecipeUtil.java
+++ ro/thehunters/digi/recipeUtil/RecipeUtil.java
@@ -14,7 +14,7 @@ import org.bukkit.inventory.ShapelessRecipe;
  * Utility class to compare Bukkit recipes.<br>
  * Useful for identifying your recipes in events, where recipes are re-generated in a diferent manner.
  * 
- * @version R1
+ * @version R1.1
  * @author Digi
  */
 public class RecipeUtil
@@ -139,7 +139,8 @@ public class RecipeUtil
             FurnaceRecipe r1 = (FurnaceRecipe)recipe1;
             FurnaceRecipe r2 = (FurnaceRecipe)recipe2;
             
-            return (r1.getInput().equals(r2.getInput())); // just compare ingredient.
+            //return (r1.getInput().equals(r2.getInput())); // TODO use this when furnace data PR is pulled
+            return r1.getInput().getTypeId() == r2.getInput().getTypeId();
         }
         else
         {
