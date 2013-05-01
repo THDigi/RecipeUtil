0b9c0150c238a9a0f36c6a46339ab14e55b4e0a8
Digi <hunter_digital_sl@yahoo.com>
2013-05-01 20:10:11 +0300

    More practical examples with good handling.

--- ro/thehunters/digi/recipeUtil/testing/Testing.java
+++ ro/thehunters/digi/recipeUtil/testing/Testing.java
@@ -1,11 +1,14 @@
 package ro.thehunters.digi.recipeUtil.testing;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.CraftItemEvent;
 import org.bukkit.event.inventory.PrepareItemCraftEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.Recipe;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -29,11 +32,53 @@ public class Testing extends JavaPlugin implements Listener
         getServer().addRecipe(myRecipe);
     }
     
+    // Quick example (message when crafting)
     @EventHandler
-    public void preCraft(PrepareItemCraftEvent event)
+    public void craft(CraftItemEvent event) // event is triggered when clicking result slot
     {
-        Recipe r = event.getRecipe();
+        // the event has some issues with getRecipe(), it still returns the last recipe when clicking the empty result slot!
+        ItemStack result = event.getCurrentItem();
+        if(result == null || result.getTypeId() == 0)
+        {
+            return;
+        }
         
-        System.out.print("(debug) recipes equal = " + RecipeUtil.areEqual(r, myRecipe));
+        // this is the part where it's checking if the current recipe is equal to the custom recipe.
+        if(RecipeUtil.areEqual(event.getRecipe(), myRecipe))
+        {
+            // And send a message to all players as an example.
+            Bukkit.broadcastMessage(event.getWhoClicked().getName() + " crafted a custom recipe making " + event.getCurrentItem() + " !");
+        }
+    }
+    
+    // Practical example (checking permissions)
+    @EventHandler
+    public void preCraft(PrepareItemCraftEvent event) // event is triggered when a recipe is found after placing ingredients
+    {
+        // Storing the equality result because it's not cheap to compare recipes.
+        // I'm doing this because I'm using it twice, if you're using it only once you don't need to store it.
+        boolean equal = RecipeUtil.areEqual(event.getRecipe(), myRecipe);
+        
+        // confirmation message of event triggering and recipe equality
+        System.out.print("(debug) recipes equal = " + equal);
+        
+        if(equal)
+        {
+            HumanEntity human = event.getView().getPlayer();
+            
+            // check if crafter has permission...
+            if(!human.hasPermission("my.awesome.permission"))
+            {
+                // basically cancels the event
+                event.getInventory().setResult(null);
+                
+                // need to check because it could be NPCs or something
+                if(human instanceof Player)
+                {
+                    Player player = (Player)human;
+                    player.sendMessage("Need permission to craft this!");
+                }
+            }
+        }
     }
 }
